30ff2bdc05758d7968b4581c3f872bf605be444b
Adam Jeliński <ajelinski@antmicro.com>
2020-12-31 02:29:46 +0100

    Add the copyright and coding information
    Signed-off-by: Adam Jeliński <ajelinski@antmicro.com>

--- update_conda_lock/update_lock.py
+++ update_conda_lock/update_lock.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+#.*coding: utf-8
+
+# Copyright (C) 2020  The SymbiFlow Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+
 
 from io import StringIO
 from os import environ
